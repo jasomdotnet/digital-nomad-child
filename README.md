c0f2755d05ce9f244e9590b57f5ec1e1cd587dbe
Jasom Dotnet <jasomdotnetpublic@gmail.com>
2020-06-04 22:33:17 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # digital-nomad-child
 Child theme for Digital Nomad WordPress theme
+
+Place 'digital-nomad-child' to your WordPress installation theme folder `/wp-content/themes`.
---
+++ digital-nomad-child/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Add child css
+ */
+function child_header_css() {
+    wp_enqueue_style( 'child', get_stylesheet_directory_uri() . '/style.css', array(), get_bloginfo( 'version' ) );
+
+}
+
+add_action( 'wp_print_styles', 'child_header_css', 100 );
+
+/**
+ * Adds Google policing
+ */
+function code_for_footer() {
+
+    ?>
+    <!-- code for footer like Google Analytics. -->
+    <?php
+
+}
+
+add_action( 'wp_footer', 'code_for_footer' );
